3aa60f8d72625764017f7c59246ee8644d6d69d2
jiangf <jiangf@wenduing.com>
2018-02-06 17:49:05 +0800

    增加数据库的实体类

--- app/src/main/java/com/boteteam/yper/yyxy/Module/FeedbackTopics.java
+++ app/src/main/java/com/boteteam/yper/yyxy/Module/FeedbackTopics.java
@@ -5,5 +5,40 @@ package com.boteteam.yper.yyxy.Module;
  */
 
 public class FeedbackTopics {
+private int idFeedbackTopics;
+private String FeedbackTopicno;
+private String FeedbackTopicanswer;
+private int Feedback_idFeedback;
 
+    public int getIdFeedbackTopics() {
+        return idFeedbackTopics;
+    }
+
+    public void setIdFeedbackTopics(int idFeedbackTopics) {
+        this.idFeedbackTopics = idFeedbackTopics;
+    }
+
+    public String getFeedbackTopicno() {
+        return FeedbackTopicno;
+    }
+
+    public void setFeedbackTopicno(String feedbackTopicno) {
+        FeedbackTopicno = feedbackTopicno;
+    }
+
+    public String getFeedbackTopicanswer() {
+        return FeedbackTopicanswer;
+    }
+
+    public void setFeedbackTopicanswer(String feedbackTopicanswer) {
+        FeedbackTopicanswer = feedbackTopicanswer;
+    }
+
+    public int getFeedback_idFeedback() {
+        return Feedback_idFeedback;
+    }
+
+    public void setFeedback_idFeedback(int feedback_idFeedback) {
+        Feedback_idFeedback = feedback_idFeedback;
+    }
 }
