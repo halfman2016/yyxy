1b39d9fffb43370d3ef7f683a0c088f4b306fd57
jiangf <jiangf@wenduing.com>
2018-02-06 15:14:12 +0800

    json库本地化

--- app/src/main/java/com/boteteam/yper/yyxy/Module/AssignMent.java
+++ app/src/main/java/com/boteteam/yper/yyxy/Module/AssignMent.java
@@ -1,9 +1,13 @@
 package com.boteteam.yper.yyxy.Module;
 
+import org.bson.BsonDateTime;
+
 /**
  * Created by jiangfeng on 2018/1/25.
  */
 
 public class AssignMent {
     private int idAssignMent;
+    private String datetime;
+
 }
