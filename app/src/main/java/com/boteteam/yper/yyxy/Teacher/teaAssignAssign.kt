86651720d2848af3f683c5819054c8ccfb7b1e3d
jiangf <jiangf@wenduing.com>
2018-04-17 12:00:36 +0800

    0417
    0417

--- app/src/main/java/com/boteteam/yper/yyxy/Teacher/teaAssignAssign.kt
+++ app/src/main/java/com/boteteam/yper/yyxy/Teacher/teaAssignAssign.kt
@@ -1,7 +1,9 @@
  package com.boteteam.yper.yyxy.Teacher
 
+import android.drm.DrmStore
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 import android.util.Log
 import com.boteteam.yper.yyxy.MyApplication
 import com.boteteam.yper.yyxy.R
@@ -18,5 +20,7 @@ import com.boteteam.yper.yyxy.R
          Log.d("myapp",gcstemp.gradeclassname)}
          var gcstemp=myapplication.gcs
          Log.d("myapp",gcstemp.count().toString())
+
+
     }
 }
